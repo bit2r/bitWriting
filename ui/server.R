cbee20caa118c8c3d4d0364a692df47c263d43bf
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2022-09-09 16:06:33 +0900

    navBar 시도... LaTeX 수식에 문제 발견

--- ui/server.R
+++ ui/server.R
@@ -12,34 +12,16 @@ library(dplyr)
 
 shinyServer(function(input, output, session) {
 
-  # *. Rmd 파일 선택하여 텍스트 보여주기
-  file_contents <- reactive(
-
-    if(input$curriculum == "확률통계") {
-      file_content <- readLines("www/stat.Rmd")
-    } else {
-      file_content <- readLines("www/test.Rmd")
-    }
-  )
-
-  output$file_content_output <- renderUI({
-      HTML(
-        paste(
-          c("<pre>", file_contents(), "</pre>"),
-          collapse = "<br>"
-        )
-      )
+  # *. 확률통계 ---------------------------
+  ## *. Rmd 편집기
+  output$stat_doc <- renderUI({
+    input$stat_eval
+    HTML(knitr::knit2html(text = isolate(input$stat_rmd), fragment.only = TRUE, quiet = TRUE)) # nolint
   })
 
-  # *. Rmd 편집기
-  output$knitDoc <- renderUI({
-    input$eval
-    HTML(knitr::knit2html(text = isolate(input$rmd), fragment.only = TRUE, quiet = TRUE)) # nolint
-  })
-
-  # *. PDF 파일 생성
-  output$report <- downloadHandler(
-    filename = "report.pdf",
+  ## *. PDF 파일 생성
+  output$stat_report <- downloadHandler(
+    filename = "report/stat_report.pdf",
     content = function(file) {
       tmp_file <- tempfile(fileext = ".Rmd")
       cat(input$rmd, file = tmp_file)
@@ -49,14 +31,31 @@ shinyServer(function(input, output, session) {
                           toc = TRUE,
                           latex_engine = "xelatex",
                           pandoc_args = c("--variable", "mainfont=NanumGothic")
-                          # 보고서 글꼴은 등은 다음과 같이 반영할 수 있습니다.
-                          # pandoc_args = c(
-                          #     "--variable", "mainfont=KoPubWorldBatang Medium",
-                          #     "--variable", "sansfont=KoPubWorldDotum Medium",
-                          #     "--variable", "linkcolor=cyan"
-                          # ),
-                          # 도커를 통해 실행시 리디바탕체를 확인할 수 있습니다.
-                          # pandoc_args = c("--variable", "mainfont=RIDIBatang"),
+                        ),
+                        envir = new.env(parent = globalenv())
+      )
+    }
+  )
+
+  # *. 수학 ---------------------------
+  ## *. Rmd 편집기
+  output$math_doc <- renderUI({
+    input$stat_eval
+    HTML(knitr::knit2html(text = isolate(input$math_rmd), fragment.only = TRUE, quiet = TRUE)) # nolint
+  })
+
+  ## *. PDF 파일 생성
+  output$stat_report <- downloadHandler(
+    filename = "report/math_report.pdf",
+    content = function(file) {
+      tmp_file <- tempfile(fileext = ".Rmd")
+      cat(input$rmd, file = tmp_file)
+      rmarkdown::render(tmp_file,
+                        output_file = file,
+                        output_format = pdf_document(
+                          toc = TRUE,
+                          latex_engine = "xelatex",
+                          pandoc_args = c("--variable", "mainfont=NanumGothic")
                         ),
                         envir = new.env(parent = globalenv())
       )
--- ui/ui.R
+++ ui/ui.R
@@ -1,59 +1,61 @@
-library(shiny)
-library(gridlayout)
-library(shinyAce)
-library(rmarkdown)
-library(shinyjs)
 
-markdown_text <- readLines("www/stat.Rmd")
-
-ui <- shinyUI(
-  fluidPage(
-    shinyjs::useShinyjs(),
-    includeCSS("www/writing.css"),
-    titlePanel(
-      fluidRow(
-        column(6, img(height = 57, width = 57, src = "dw_logo.png"),
-        column(6, "디지털 글쓰기", class = "pull-right")
-        )
-      )
-    ),
-    sidebarLayout(
-      sidebarPanel(
-        width = 2,
-        h3("교과목"),
-        selectInput("curriculum",
-                    "글쓰기 분야",
-                    choices = c("확률통계", "수학", "국어", "과학"))
-
-      ),
-      mainPanel(
-        width = 10,
-        h2("Markdown Editor"),
-        hr(),
-        column(
-          6,
-          h2("R-Markdown Editor"),
-          aceEditor("rmd",
-                    mode = "rmarkdown",
-                    tabSize = 4,
-                    useSoftTabs = TRUE,
-                    value = markdown_text
-          ),
-          actionButton("eval", "Update"),
-          downloadButton("report", "보고서"),
-          uiOutput("file_content_output")
-        ),
-        column(
-          6,
-          h2("미리 보기"),
-          htmlOutput("knitDoc")
-        )
-      )
-    ),
-    div(
-      class = "footer",
-      includeHTML("www/footer.html")
-    )
-  )
+ui <- navbarPage(
+  tags$head(
+    tags$link(href = "quarto.css", rel = "stylesheet", type = "text/css")
+  ),
+  title = "디지털 글쓰기",
+  tabPanel("확률통계",
+     fluidPage(
+         mainPanel(
+           column(
+             6,
+             h2("글쓰기 편집기"),
+             aceEditor("stat_rmd",
+                       mode = "rmarkdown",
+                       tabSize = 4,
+                       useSoftTabs = TRUE,
+                       value = stat_text
+             ),
+             actionButton("stat_eval", "미리보기"),
+             downloadButton("stat_report", "보고서")
+           ),
+           column(
+             6,
+             h2("미리 보기"),
+             htmlOutput("stat_doc")
+           )
+         )
+       )
+  ),
+  tabPanel("수학",
+     fluidPage(
+       mainPanel(
+         column(
+           6,
+           h2("글쓰기 편집기"),
+           aceEditor("math_rmd",
+                     mode = "rmarkdown",
+                     tabSize = 4,
+                     useSoftTabs = TRUE,
+                     value = math_text
+           ),
+           actionButton("math_eval", "미리보기"),
+           downloadButton("math_report", "보고서")
+         ),
+         column(
+           6,
+           h2("미리 보기"),
+           htmlOutput("math_doc")
+         )
+       )
+     )
+  ),
+  tabPanel("국어", "korean"),
+  navbarMenu("사회 & 과학",
+             tabPanel("지리", "geospatial"),
+             tabPanel("물리", "physics"),
+             tabPanel("사회경제", "social")
+  ),
+  inverse = TRUE
 )
 
