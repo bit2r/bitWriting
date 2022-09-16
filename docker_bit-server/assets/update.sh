6b141775e9b55398d78a6bfe78d461b47428f8ba
joygram <joygram@vividstudio.co.kr>
2022-09-16 22:34:27 +0900

    update.sh

--- docker_bit-server/assets/update.sh
+++ docker_bit-server/assets/update.sh
@@ -15,6 +15,6 @@ fi
 cp /home/app/shiny-server/docker_bit-server/assets/shiny-server.conf /etc/shiny-server/shiny-server.conf
 
 # update r-packages 
-R -f /home/app/install_packages.R
+sudo R -f /home/app/install_packages.R
 
 /init 
