8ace0a261bddceeb6c83f57619a15369d0754c29
joygram <joygram@vividstudio.co.kr>
2022-09-17 08:49:30 +0900

    split baking script and runtime script

--- docker_bit-server/assets/update.sh
+++ docker_bit-server/assets/update.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/bash
 
-
-if [ ! -d shiny-server ]; then
-	echo git clone 
-	sudo -u shiny git clone https://github.com/bit2r/bitWriting.git /home/app/shiny-server
-else 
-	echo git update 
-	cd /home/app/shiny-server
-	sudo -u shiny git reset --hard
-	sudo -u shiny git pull 
-fi
-
 cd /home/app/shiny-server/docker_bit-server/assets
+source ./update_repository.sh
 source ./run_server.sh
