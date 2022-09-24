b3bb4ee0f98127ab190b5bfc09e606f60f4bda6b
joygram <joygram@gmail.com>
2022-09-24 18:21:35 -0400

    update bit-server

--- docker_bit-server/scripts/run_server.sh
+++ docker_bit-server/scripts/run_server.sh
@@ -6,6 +6,6 @@ cp ./shiny-server.conf /etc/shiny-server/shiny-server.conf
 # update r-packages 
 #R -f ./install_packages_additional.R
 cd /home/shiny-server
-R -e "renv::restore()"
+R -e "renv::init()"
 
 /init 
