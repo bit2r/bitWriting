94aabe18c1d4a0c4e21d270bd00a6149290a6fc8
joygram <joygram@vividstudio.co.kr>
2022-09-25 12:35:01 +0900

    run_server.sh

--- docker_bit-server/scripts/run_server.sh
+++ docker_bit-server/scripts/run_server.sh
@@ -7,10 +7,10 @@ cp ./shiny-server.conf /etc/shiny-server/shiny-server.conf
 #R -f ./install_packages_additional.R
 
 # change renv global path by joygram 2022/09/25
-RENV_PATHS_ROOT=/home/shiny-server/share
-export RENV_PATHS_ROOT
+RENV_PATHS_CACHE=/home/shiny-server/renv/library/R-4.2/x86_64-pc-linux-gnu
+export RENV_PATHS_CACHE
 
-echo envroot[$RENV_PATHS_ROOT]
+echo envroot[$RENV_PATHS_CACHE]
 cd /home/shiny-server
 R -e "renv::restore(exclude = c(\"BitStat\"))"
 R -e "renv::status()"
