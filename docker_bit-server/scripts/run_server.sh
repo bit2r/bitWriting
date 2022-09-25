3b21e8ad747be11925c6d9de5379b38dbecb7c56
joygram <joygram@vividstudio.co.kr>
2022-09-25 11:58:42 +0900

    update run_server.sh

--- docker_bit-server/scripts/run_server.sh
+++ docker_bit-server/scripts/run_server.sh
@@ -6,6 +6,8 @@ cp ./shiny-server.conf /etc/shiny-server/shiny-server.conf
 # update r-packages 
 #R -f ./install_packages_additional.R
 cd /home/shiny-server
-R -e "renv::restore()"
+R -e "renv::restore(confirm=TRUE)"
+R -e "renv::status()"
+
 
 /init 
